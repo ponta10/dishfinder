b32d11f5a2e91490ecf08911d409854281e6f8b8
ponta10 <mponta2002@gmail.com>
2023-09-28 20:54:43 +0900

    storybook修正

--- components/organisms/SearchBox/SearchBox.stories.tsx
+++ components/organisms/SearchBox/SearchBox.stories.tsx
@@ -15,5 +15,5 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  render: () => <SearchBox setSearch={() => {}} />,
+  render: () => <SearchBox setSearch={() => {}} setFocus={() => {}} />,
 }
