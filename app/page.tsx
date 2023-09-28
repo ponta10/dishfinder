6901be40b16ae6849fadc6b61733a684fcf575bb
ponta10 <mponta2002@gmail.com>
2023-09-28 23:09:02 +0900

    window削除

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Top } from '@/components/pages/Top'
 import { useState } from 'react'
 
 export default function Home() {
-  const width = window.innerWidth;
+  const width = innerWidth;
   const isMobile = width <= 768;
   const [search, setSearch] = useState<boolean>(false)
   const [focus, setFocus] = useState<string>('')
