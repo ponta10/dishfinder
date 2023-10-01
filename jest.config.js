50bc9f14b1770056ded6347776c722538d5b032e
ponta10 <mponta2002@gmail.com>
2023-10-01 22:28:23 +0900

    lint修正

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'], 
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 }
 
 module.exports = createJestConfig(customJestConfig)
