80d1cab341f59132225770a3a46932a84bf3bda5
ponta10 <mponta2002@gmail.com>
2023-10-21 00:29:14 +0900

    storybook不足分追加

---
+++ components/pages/Error/Error.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Error } from ".";
+
+const meta: Meta = {
+  title: "Example/Error",
+  component: Error,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
