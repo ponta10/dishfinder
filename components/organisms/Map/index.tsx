a73e57a242e54533bce187d02e256f28fb0624eb
ponta10 <mponta2002@gmail.com>
2024-03-26 17:34:17 +0900

    地図階層変更

--- components/organisms/Map/index.tsx
+++ components/organisms/Map/index.tsx
@@ -19,6 +19,7 @@ const MapContainer = styled.div`
   bottom: 0;
   right: 0;
   text-align: right;
+  z-index: 5;
 `
 
 interface MapProps {
@@ -32,6 +33,7 @@ export const Map: React.FC<MapProps> = ({ items }) => {
     googleMapsApiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
     libraries: ['geometry', 'drawing'],
   })
+  console.log(items)
   if (!isLoaded) return
   return (
     <MapContainer>
@@ -40,7 +42,7 @@ export const Map: React.FC<MapProps> = ({ items }) => {
         text={map ? '地図を非表示にする' : '地図を表示する'}
         onClick={() => setMap(!map)}
       ></Button>
-      {map && (
+      {map && items.length > 0 && (
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={{ lat: items[0]?.location.lat, lng: items[0]?.location.lng }}
